a60c5207b90f189eb55175757fd8de3378bbba88
Bilguun0118 <bi7guunt@gmail.com>
2021-11-11 14:27:51 +0800

    bair-1

---
+++ bair.js
@@ -0,0 +1,27 @@
+let bair = document.querySelector('.bair');
+let orts = document.querySelectorAll('.orts');
+let haalga = document.querySelectorAll('.haalga');
+let davhar = document.querySelectorAll('.davhar');
+let tsonh = document.querySelectorAll('.tsonh');
+
+for (let i = 0; i < 3; i++) {
+    neworts = document.createElement("div");
+    neworts.classList.add('orts');
+    bair.append(neworts);
+    for (let i = 0; i < 10; i++) {
+        newdavhar = document.createElement("div");
+        newdavhar.classList.add('davhar');
+        neworts.append(newdavhar);
+        for (let i = 0; i < 5; i++) {
+            newtsonh = document.createElement("div");
+            newtsonh.classList.add('tsonh');
+            newdavhar.append(newtsonh);
+
+        }
+    }
+}//for (let i=0; i<1; i++){
+
+    //newhaalga = document.createElement("div");
+    //newhaalga.classList.add('orts');
+    //neworts.append(newhaalga);
+//}
